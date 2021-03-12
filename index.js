6f1640d4c2f861727be7ee5f28f1ee0e03b88d60
Abhishek Nair <abhisheknr4@gmail.com>
2021-03-12 15:19:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const ProgressBar = props => {
-  const { percentage, primary, secondary, style } = props;
+  const { percentage, primary, secondary, style, primaryStyles } = props;
   const styles = StyleSheet.create({
     container: {
       height: 10,
       backgroundColor: secondary,
       width: '100%',
-      borderRadius: 6
+      borderRadius: 6,
+      height: 10
     },
     progress: {
-      backgroundColor: primary,
-      width: `${percentage}%`,
+        backgroundColor: primary,
+        width: `${percentage}%`,
+        height: 10
     }
   });
   return (
     <View style={{...styles.container,...style}}>
-      <View style={{...styles.progress,...styles.primary}} />
+      <View style={{...styles.progress,...primaryStyles}} />
     </View>
   );
 };
